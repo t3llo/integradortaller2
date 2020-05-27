a99c2de32b4065ace56a9bbd922e7483bac99069
t3llo <lolwtfxd49@gmail.com>
2020-05-27 15:22:17 -0500

    funcionando

--- humedadAire/views.py
+++ humedadAire/views.py
@@ -7,10 +7,13 @@ def humedadAire(request):
     # Verifica si hay un parámetro value en la petición GET
     if 'value' in request.GET:
         value = request.GET['value']
+        latitud = request.GET['latitude']
+        longitud = request.GET['longitude']
+        terreno = request.GET['terrain']
         # Verifica si el value no esta vacio
         if value:
             # Crea el json para realizar la petición POST al Web Service
-            args = {'value': value}
+            args = {"type": 'humedad', 'value': value, 'latitude': latitud, 'longitude': longitud, 'terrain' : terreno }
             response = requests.post('http://127.0.0.1:8000/humedadAire/', args)
             # Convierte la respuesta en JSON
             humedadAire_json = response.json()
